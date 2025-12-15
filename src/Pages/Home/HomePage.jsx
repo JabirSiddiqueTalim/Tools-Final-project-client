7acaaa8fc1568dfdb680e683a3c71a9cdae77ec7
MasadRayan <masadrayan2002@gmail.com>
2025-12-16 04:31:23 +0600

    FAQ section added

--- src/Pages/Home/HomePage.jsx
+++ src/Pages/Home/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import HeroSection from './Components/HeroSection';
+import FAQ from './Components/FAQ';
 
 const HomePage = () => {
     return (
         <div>
             <HeroSection></HeroSection>
+            <FAQ></FAQ>
         </div>
     );
 };
