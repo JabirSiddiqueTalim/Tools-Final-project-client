14f8ad607ad6543c8b97adc4f163f3cbd9b83e68
MasadRayan <masadrayan2002@gmail.com>
2025-12-16 13:49:55 +0600

    Added Toster in the main file

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,14 @@ import './index.css'
 import { RouterProvider } from 'react-router'
 import { router } from './router/Router.jsx'
 import AuthProvider from './Context/AuthProvider.jsx'
+import { Toaster } from 'react-hot-toast'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
       <RouterProvider router={router}>
-
       </RouterProvider>
+      <Toaster></Toaster>
     </AuthProvider>
   </StrictMode>,
 )
