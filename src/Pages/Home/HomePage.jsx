e7538b2c5f8a79943de3d5ea4d96547270e45625
MasadRayan <masadrayan2002@gmail.com>
2025-12-16 16:44:32 +0600

    FeaturesSection created

--- src/Pages/Home/HomePage.jsx
+++ src/Pages/Home/HomePage.jsx
@@ -4,15 +4,19 @@ import FAQ from './Components/FAQ';
 import NewsletterSection from './Components/NewsletterSection';
 import StatsSection from './Components/StatsSection';
 import BrandMarquee from './Components/BrandMarquee';
+import FeaturesSection from './Components/FeaturesSection';
+import { ScrollRestoration } from 'react-router';
 
 const HomePage = () => {
     return (
         <div>
             <HeroSection></HeroSection>
+            <FeaturesSection></FeaturesSection>
             <BrandMarquee></BrandMarquee>
             <StatsSection></StatsSection>
             <FAQ></FAQ>
             <NewsletterSection></NewsletterSection>
+            <ScrollRestoration></ScrollRestoration>
         </div>
     );
 };
